2ba43fdbd4bfb3eeb4ba846f5ea81924c32486c9
jk7830a <jk7830a@american.edu>
2024-09-26 00:25:05 -0400

    flightsimulation2
    My Second Attempt at a Flight Simulation. I tried to salvage first, it just did not work.

--- assignments/FlightSimulation/Assets/PlaneScript.cs
+++ assignments/FlightSimulation/Assets/PlaneScript.cs
@@ -1,120 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
 
 public class PlaneScript : MonoBehaviour
 {
-    public GameObject cameraObject;
+    float forwardSpeed = 0.01f;
 
-    public Terrain terrain; 
-
-    //These variables control how the plane moves!
-     float forwardSpeed = 30f; // NEW for times Time.deltaTime
-    
-    float xRotationSpeed = 90f; // NEW for times Time.deltaTime
-
-    float yRotationSpeed = 90f; // NEW for times Time.deltaTime
-
-    private Vector3 startingPosition;
-    public float speedIncrease = 5f;
-
-    
     // Start is called before the first frame update
     void Start()
     {
-        startingPosition = transform.position;
-	AdjustCameraToSeeAll(); // suggested by ChatGPT
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get directional input (up, down, left, right)
-        float vAxis = Input.GetAxis("Vertical"); // -1 if down is pressed, 1 if up is pressed, 0 if neither is pressed
-        float hAxis = Input.GetAxis("Horizontal"); //-1 if left is pressed, 1 if right is pressed, 0 if neither is pressed
-
-        //float push = Input.GetKey("Spacebar") //if spacebar is pressed
-
-        //Deal with colliding with the terrain
-        float terrainHeight = terrain.SampleHeight(transform.position);
-            //Debug.Log(terrainHeight);
-            //Debug.Log(transform.position.y);
-        if (transform.position.y < terrainHeight){
-            Vector3 correctedPosition = transform.position;
-            correctedPosition.y = terrainHeight; // Ensure plane stays above terrain
-            transform.position = correctedPosition;
-        }
-
-        //Apply the rotation based on the inputs!
-        Vector3 amountToRotate = new Vector3(0,0,0);
-        amountToRotate.x = vAxis * xRotationSpeed;  
-        amountToRotate.y = hAxis * yRotationSpeed;
-        amountToRotate *= Time.deltaTime; //amountToRotate = amountToRotate * Time.deltaTime;
-        transform.Rotate(amountToRotate, Space.Self);
-
-
-        //Make the plane move forward by adding the forward vector to the position.
-        transform.position += transform.forward * forwardSpeed * Time.deltaTime;
-
-
-
-        //Position the camera!
-        Vector3 cameraPosition = transform.position;
-        cameraPosition += -transform.forward * 300f;
-        cameraPosition += Vector3.up * 150f;  
-        cameraObject.transform.position = cameraPosition;
-
-        cameraObject.transform.LookAt(transform.position);
-    }
-void AdjustCameraToSeeAll()
-    {
-        // Get terrain size and position
-        Vector3 terrainSize = terrain.terrainData.size;
-        Vector3 terrainPosition = terrain.transform.position;
-
-        // Adjust the camera to show the entire terrain
-        Vector3 cameraPosition = new Vector3(
-            terrainPosition.x + terrainSize.x / 2,
-            terrainPosition.y + terrainSize.y + 100f,
-            terrainPosition.z - terrainSize.z / 2
-        );
+        transform.position += transform.position * forwardSpeed;
         
-        cameraObject.transform.position = cameraPosition;
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject.name);
-        if (other.CompareTag("collectable"))
-        {
-            Destroy(other.gameObject);
-        } 
-        else if (other.CompareTag("obstacle")) //crashes with birds
-        {
-            Crash(); 
-        }
-        else if (other.CompareTag("gas")) //speed increase if collects gas
-        {
-            IncreaseSpeed(); 
-            Destroy(other.gameObject);
-        }
-        else if (other.CompareTag("wall"))
-        {
-            transform.Rotate(0, 180, 0, Space.World); // if player (plane) moves off the map
-        }
-
-    }
-
-    private void Crash()
-    {
-        transform.position = startingPosition;
-        forwardSpeed = 0;
-        Debug.Log("Crashed into a bird!");
-    }
-    private void IncreaseSpeed()
-    {
-        forwardSpeed += speedIncrease;
     }
 }
---
+++ assignments/FlightSimulation2/Assets/SpeedScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedScript : MonoBehaviour
+{
+    // code from - https://www.youtube.com/watch?v=C7sPsksH4JM w/ suggestions from ChatGPT
+    float amp = 0.9f;
+    float freq = 2f;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * freq ) * amp;
+        transform.localPosition = new Vector3(startPosition.x, newY, startPosition.y );
+    }
+}
