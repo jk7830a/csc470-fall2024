2f7c6294e61e62d45c915d3735f4cca995b7ff28
jk7830a <jk7830a@american.edu>
2024-10-15 03:43:21 -0400

    emergence

--- assignments/Emergence/Assets/GameManager.cs
+++ assignments/Emergence/Assets/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     CellScript[,] grid;
     float spacing = 1.1f;
 
+    public int score = 0; 
+
     bool[,] blockPattern = new bool[,]  //help from wiki and chat gpt
     {
         { true, true },
